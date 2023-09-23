566164414fbf8e40ffdb9f0d2df36c2d8d793d62
Armandase <damiens.armand@gmail.com>
2023-09-23 09:22:57 +0200

    render player filled'

--- src/drawCircle.cpp
+++ src/drawCircle.cpp
@@ -1,31 +1,51 @@
 #include "../inc/gomoku.hpp"
 
 void drawCircle(int centreX, int centreY, SDL_Renderer *renderer){
-    int x = RADIUS - 1;
-    int y = 0;
-    int distX = 1, distY =1;
-    int err = distX - (RADIUS * 2);
+    // int x = RADIUS - 1;
+    // int y = 0;
+    // int distX = 1, distY =1;
+    // int err = distX - (RADIUS * 2);
 
+    // SDL_SetRenderDrawColor(renderer, 80, 0, 80, 255);
+    // while (x >= y){
+    //     // SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
+    //     // SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
+    //     // SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
+    //     // SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
+    //     // SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
+    //     // SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
+    //     // SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
+    //     // SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+
+    //     SDL_RenderDrawLine(renderer, centreX + x, centreY + y, centreX - x, centreY + y);
+    //     SDL_RenderDrawLine(renderer, centreX + x, centreY - y, centreX - x, centreY - y);
+
+    //     SDL_RenderDrawLine(renderer, centreX + x, centreY + y, centreX + x, centreY - y);
+    //     SDL_RenderDrawLine(renderer, centreX - x, centreY + y, centreX - x, centreY - y);
+    //     if (err <= 0){
+    //         y++;
+    //         err += distY;
+    //         distY += 2;
+    //     }
+
+    //     if (err > 0){
+    //         x--;
+    //         distX += 2;
+    //         err += (distX - (RADIUS*2));
+    //     }
+    // }
+    std::cout << "X:" << centreX << "\n";
     SDL_SetRenderDrawColor(renderer, 80, 0, 80, 255);
-    while (x >= y){
-        SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
-        SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
-        SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
-        SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
-        SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
-        SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
-        SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
-        SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
-        if (err <= 0){
-            y++;
-            err += distY;
-            distY += 2;
-        }
-
-        if (err > 0){
-            x--;
-            distX += 2;
-            err += (distX - (RADIUS*2));
+    for (int w = 0; w < RADIUS * 2; w++)
+    {
+        for (int h = 0; h < RADIUS * 2; h++)
+        {
+            int dx = RADIUS - w; // horizontal offset
+            int dy = RADIUS - h; // vertical offset
+            if ((dx*dx + dy*dy) <= (RADIUS * RADIUS))
+            {
+                SDL_RenderDrawPoint(renderer, centreX + dx, centreY + dy);
+            }
         }
     }
     SDL_RenderPresent(renderer);
