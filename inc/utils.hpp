de4f1286aa97ba5a865638cb63678488fc1a9553
Armandase <adamiens@student.42angouleme.fr>
2023-09-25 16:59:14 +0200

    add write text function

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -2,6 +2,8 @@
 # define UTILS_HPP
 # include "gomoku.hpp"
 
-void SDL_Error(const std::string &, SDL_Window *, SDL_Renderer *);
+void    SDL_Error(const std::string &, SDL_Window *, SDL_Renderer *);
+void    writeText(const std::string&, const std::string&, const SDL_Rect*, const SDL_Color&, SDL_Renderer *);
+
 
 #endif
