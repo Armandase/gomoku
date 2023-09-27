6d916b5c719b42ef022eddae78e21d7a25cd5bd1
Armandase <adamiens@student.42angouleme.fr>
2023-09-27 16:59:03 +0200

    add heuristic every where

--- src/heuristic.cpp
+++ src/heuristic.cpp
@@ -67,7 +67,7 @@ int counterAnalysis(const int counter, const int player, const int color){
     if (counter == 5 && player == color)
         result = 1000;
     else if (counter == 5 && player != color)
-        result = 1000;
+        result = 900;
     else if (counter == 4 && player != color)
         result = 600;
     else if (counter == 4 && player == color)
@@ -87,24 +87,30 @@ int heuristic(const vector2d &game, int player, const int y, const int x){
     int         checkX = 0, checkY = 0;
     int         counter, color, heuristic = 0;
 
-    for (int i = 0; i < 8; i++){
-        counter = 1;
-        color = 0;
-        for (int j = 1; j < 5; j++){
-            checkX = x + (dirX[i] * j);
-            checkY = y + (dirY[i] * j);
-            if (checkX < 0 || checkY < 0 || checkX > BOARD_SIZE || checkY > BOARD_SIZE || game[checkY][checkX] == 0)
-                break ;
-            if (j == 1)
-                color = game[checkY][checkX];
-            if (game[checkY][checkX] == color && counter + 1 < 5)
-                counter++;
-            else
-                break ;
+    for (int y = 0; y < BOARD_SIZE; y++){
+        for (int x = 0; x < BOARD_SIZE; x++){
+            if (game[y][x] == 0)
+                continue ;
+            for (int i = 0; i < 8; i++){
+                counter = 1;
+                color = 0;
+                for (int j = 1; j < 5; j++){
+                    checkX = x + (dirX[i] * j);
+                    checkY = y + (dirY[i] * j);
+                    if (checkX < 0 || checkY < 0 || checkX > BOARD_SIZE || checkY > BOARD_SIZE || game[checkY][checkX] == 0)
+                        break ;
+                    if (j == 1)
+                        color = game[checkY][checkX];
+                    if (game[checkY][checkX] == color && counter + 1 < 5)
+                        counter++;
+                    else
+                        break ;
+                }
+                if (color)
+                    heuristic += counterAnalysis(counter + 1, player, color);
+            }
         }
-        if (color)
-            heuristic += counterAnalysis(counter + 1, player, color);
     }
-    // std::cout << "HEURISITIC: " << heuristic << "\n";
+    std::cout << "HEURISITIC: " << heuristic << "\n";
     return (heuristic);
 }
