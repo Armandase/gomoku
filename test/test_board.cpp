2a533293445117e2045a918ad337d6e69e7d38fc
Armandase <adamiens@student.42angouleme.fr>
2024-10-16 11:47:46 +0200

    add fcuntion to test pattern extrction

--- test/test_board.cpp
+++ test/test_board.cpp
@@ -115,68 +115,42 @@ TEST(IBoard, TestDiagBoard){
 
     EXPECT_EQ(gameTest.getDiagBoard().getPlayer1(), waited_result);
 }
-// TEST(Board_extractPatterns, TestInputExtraction){
-//     Board::bitboard test(
-// "0000000000000000000"
-// "0000000000000000000"
-// "0000000000000000000"
-// "0001001000000000000"
-// "0000000000000000000"
-// "0000011000000000000"
-// "0001011000000000000"
-// "0000011000000000000"
-// "0000000000000000000"
-// "0001001000000000000"
-// "0000000000000000000"
-// "0000000000000000000"
-// "0000000000000000000"
-// "0000000000000000000"
-// "0000000000000000000"
-// "0000000000000000000"
-// "0000000000000000000"
-// "0000000000000000000"
-// "0000000000000000000");
 
-//     Board gameTest;
-//     int player = 0;
-//     for (int i = 0; i < test.size(); i++){
-//         if (!test[i])
-//             continue;
-//         std::cout << "Pos to place x:" << i % gameTest.getWidth() << " y: " << i / gameTest.getWidth() << std::endl;
-//         gameTest.setPos(i % gameTest.getWidth(), i / gameTest.getWidth(), test[i]);
-//     }
-    
-//     Board::patternMap waitedResult;
-//     patternBitset pattern("1011");
-//     waitedResult.insert(Board::patternPair(Board::PatternType::DEFAULT,pattern));
-//     waitedResult.insert(Board::patternPair(Board::PatternType::TRANSPOS, pattern));
-//     waitedResult.insert(Board::patternPair(Board::PatternType::DIAG, pattern));
-//     waitedResult.insert(Board::patternPair(Board::PatternType::ANTIDIAG, pattern));
+TEST(ClassicBoard, extractPattern){
+    IBoard::bitboard test(
+"0000000000000000000"
+"0101100000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000"
+"0000000000000000000");
 
-//     EXPECT_EQ(gameTest.extractPatterns(19-7, 19-7, 5, 1), waitedResult);
-// }
+    ClassicBoard board;
 
-// Custom listener that triggers on failure of specific tests
-class FailListener : public ::testing::TestEventListener {
- public:
-  // Override the method to check for test results
-  void OnTestPartResult(const ::testing::TestPartResult& result) override {
-    if (result.failed()) {
-      const ::testing::TestInfo* test_info =
-          ::testing::UnitTest::GetInstance()->current_test_info();
-      if (test_info != nullptr) {
-        std::string test_name = std::string(test_info->test_suite_name()) + "." + test_info->name();
-        
-        // Specify the test you want to call the custom function for
-        if (test_name == "IBoard.TestTransposedBoard") {
-          OnTestFailure();
-        }
-      }
+    int player = 0;
+    int width = board.getWidth();
+    for (int i = 0; i < test.size(); i++){
+        if (!test[i])
+            continue;
+        board.setPos(i % width, i / width, test[i]);
     }
-  }
+    
+    board.printBoard();
+    patternBitset  waitedResult("1011");
 
-  void OnTestFailure() {
-
-  }
-};
+    EXPECT_EQ(board.extractPattern(1, 1, 4, 1), waitedResult);
+}
 
